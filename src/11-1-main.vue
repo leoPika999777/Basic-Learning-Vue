a00fe0f1f3a51e8a3357236147514fa856486625
leoPika999777 <147688987+leoPika999777@users.noreply.github.com>
2024-03-12 17:25:20 +0800

    切換function

--- src/11-1-main.vue
+++ src/11-1-main.vue
@@ -1,17 +1,18 @@
 <script setup>
 import { ref } from "vue";
-const text = ref("不同組件呼叫function  方法一");
+const textA = "不同組件呼叫function  方法一";
+const textB = "不同組件呼叫function  Way1";
+let text = ref(textA);
+
 let change = function () {
-  text.value = "不同組件呼叫function  Way1";
+  console.log(text);
+  text.value == textA ? (text.value = textB) : (text.value = textA);
 };
 
 defineProps(["newcolor", "BGcolor"]);
 </script>
 <template>
-  <main
-    :style="
-  {color:newcolor , backgroundColor:BGcolor}"
-  >
+  <main :style="{ color: newcolor, backgroundColor: BGcolor }">
     <div @click="change">{{ text }}</div>
     <button @click="$emit('update')">改變副標題A</button>
   </main>
--- src/11-Nav.vue
+++ src/11-Nav.vue
@@ -5,8 +5,8 @@ defineProps(["title", "subtitleA", "subtitleB"]);
 <template>
   <nav>
     <div>{{ title }}</div>
-    <div>{{ subtitleA }}</div>
-    <div>{{ subtitleB }}</div>
+    <div class="subtitleA" >{{ subtitleA }}</div>
+    <div class="subtitleB" >{{ subtitleB }}</div>
   </nav>
 </template>
 <style scoped>
@@ -14,5 +14,15 @@ nav {
   background-color: rgb(231, 97, 15);
   margin: 5px;
   font-size: 36px;
+}
+.subtitleA{
+  color:white;
+  font-size: 50px;
+  background-color: rgb(62, 15, 231);
+}
+.subtitleB{
+  color:white;
+  font-size: 50px;
+  background-color: rgb(2, 54, 31);
 }
 </style>
